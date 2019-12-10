4fff001f1b53dee5f71dea7c366f6a51e1f6c7d5
adkhiker <poirier.greg@gmail.com>
2019-12-10 18:13:38 -0500

    WIP: Private routing

--- friends/src/utils/PrivateRoute.js
+++ friends/src/utils/PrivateRoute.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import {Route, Redirect} from 'react-router-dom';
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-    return (
-      <Route
-        {...rest}
-        render={props => {
-          if (localStorage.getItem('token')) {
-            return <Component {...props} />;
-          } else {
-            return <Redirect to= {{
-                pathname: '/login',
+  return (
+    <Route
+      {...rest}
+      render={props => {
+        if (localStorage.getItem("token")) {
+          return <Component {...props} />;
+        } else {
+          return (
+            <Redirect
+              to={{
+                pathname: "/login",
                 state: { from: props.location }
-            }} />
-          }
-        }}
-      />
-    );
-  };
-  
-  export default PrivateRoute;
-  
+              }}
+            />
+          );
+        }
+      }}
+    />
+  );
+};
+
+export default PrivateRoute;
