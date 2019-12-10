26c444c339e567385c37054b83c716c1f8cd4ce0
adkhiker <poirier.greg@gmail.com>
2019-12-10 18:46:35 -0500

    WIP: Friends forms and login

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const Friend = props => {
-    return (
-        <div className="list">
-            <p>Name: {props.friend.name}</p>
-            <p>Age: {props.friend.age}</p>
-            <p>Email: {props.friend.email}</p>
-        </div>
-    )
-}
+  return (
+    <div className="list">
+      <p>Name: {props.friend.name}</p>
+      <p>Age: {props.friend.age}</p>
+      <p>Email: {props.friend.email}</p>
+    </div>
+  );
+};
 export default Friend;
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,45 +1,40 @@
-import React, {Component} from 'react';
-import {axiosWithAuth} from '../utils/axiosWithAuth'
-import DashboardStyle from '../styling/dashboardstyle'
+import React, { Component } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import DashboardStyle from "../styling/dashboardstyle";
 
 //Components
-import Friend from './Friend'
-
+import Friend from "./Friend";
 
 class Friends extends Component {
-state = {
-   friends: []
-}
+  state = {
+    friends: []
+  };
 
-componentDidMount() {
+  componentDidMount() {
     this.getData();
-}
+  }
 
-getData = () => {
+  getData = () => {
     axiosWithAuth()
-    .get('/api/friends')
-    .then(res => {
-        this.setState( {
-            friends: res.data
-        })
-    })
-    .catch(err => console.log(err.response))
-}
-    render()  {
-        return(
-            <DashboardStyle>
-                <h1>Friends</h1>
-                {this.state.friends.map(friend => (
-                    <div>
-                    <Friend 
-                    key= {friend.id}
-                    friend= {friend}
-                    />
-                    </div>
-                ))}    
-            </DashboardStyle>
-        )
-    }
-    
+      .get("/api/friends")
+      .then(res => {
+        this.setState({
+          friends: res.data
+        });
+      })
+      .catch(err => console.log(err.response));
+  };
+  render() {
+    return (
+      <DashboardStyle>
+        <h1>Friends</h1>
+        {this.state.friends.map(friend => (
+          <div>
+            <Friend key={friend.id} friend={friend} />
+          </div>
+        ))}
+      </DashboardStyle>
+    );
+  }
 }
 export default Friends;
