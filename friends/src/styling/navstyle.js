2915a05e4b68041d1561e10d947c4befad4e09cc
adkhiker <poirier.greg@gmail.com>
2019-12-10 17:41:53 -0500

    WIP: Packages

---
+++ friends/src/styling/navstyle.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+const NavStyle = styled.div`
+  nav {
+    border: 3px solid #8a4f7d;
+    
+    margin: 0 0 3% 0;
+    padding: 2%;
+    background: #88a096;
+  }
+
+  nav a {
+    text-decoration: none;
+    margin: 5%;
+    color: black;
+    :hover {
+      color: white;
+      font-weight: bold;
+    }
+  }
+`;
+export default NavStyle;
